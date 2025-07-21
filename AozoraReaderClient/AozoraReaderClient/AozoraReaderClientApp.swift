1ed3ece72d40290377f41d3d2db113f6d98718fa
TEM <highhigheggman@gmail.com>
2025-07-21 12:55:26 +0900

    iOSプロジェクトを作成

---
+++ AozoraReaderClient/AozoraReaderClient/AozoraReaderClientApp.swift
@@ -0,0 +1,17 @@
+//
+//  AozoraReaderClientApp.swift
+//  AozoraReaderClient
+//
+//  Created by yoshiki-tx on 2025/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AozoraReaderClientApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

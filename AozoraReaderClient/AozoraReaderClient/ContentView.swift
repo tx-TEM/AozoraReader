52cfd517e43c64185c2c163802a0feb6537cae83
yoshiki-tx <yoshiki-tx@yoshiki-txnoMac-mini.local>
2025-07-21 12:51:01 +0900

    iosプロジェクトを作成

---
+++ AozoraReaderClient/AozoraReaderClient/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  AozoraReaderClient
+//
+//  Created by yoshiki-tx on 2025/07/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ AozoraReaderClient/AozoraReaderClientTests/AozoraReaderClientTests.swift
@@ -0,0 +1,16 @@
+//
+//  AozoraReaderClientTests.swift
+//  AozoraReaderClientTests
+//
+//  Created by yoshiki-tx on 2025/07/21.
+//
+
+import Testing
+
+struct AozoraReaderClientTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
